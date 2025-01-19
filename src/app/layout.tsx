8aa4785fcfff8b604b8565ef00ada3df96d8ab6d
teracores <teracores1@gmail.com>
2025-01-19 15:23:38 +0545

    Quatation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,7 +71,7 @@ export default function RootLayout({
           <div className="fixed bottom-4 left-4 w-80 bg-gradient-to-r from-[#1b1b1b] to-[#292929] border-2 border-[#353535] text-white p-4 rounded-lg z-50">
             <p className="text-sm">
               We use cookies for marketing and analytics purposes. By clicking
-              "Accept", you agree to the use of cookies.
+              &quot;Accept&quot;, you agree to the use of cookies.
             </p>
             <div className="flex space-x-4 mt-4">
               <button
