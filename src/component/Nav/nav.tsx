64916a9a0b3adc2beeb94f720fbd41e6a5493be7
teracores <teracores1@gmail.com>
2025-02-02 16:41:08 +0545

    commit

--- src/component/Nav/nav.tsx
+++ src/component/Nav/nav.tsx
@@ -74,7 +74,7 @@ const Nav = () => {
             className="w-5 h-5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
+            fill="black"
             viewBox="0 0 17 14"
           >
             <path
